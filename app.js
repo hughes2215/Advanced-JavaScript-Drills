932bc8ef640cab3d1e2d1a4f0b300dbe28adac88
hughes2215 <hughes2215@gmail.com>
2018-06-06 17:26:08 -0500

    completed

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
 //Hoisting and Functions below
-name  = 'Billy';
+name = "Billy";
 var name;
 
 console.log(name);
 
 setName();
-function setName(){
-    setName();
 function setName() {
-    var name = 'Covalence';
+  setName();
+  function setName() {
+    var name = "Covalence";
     console.log(name);
+  }
+
+  //Average
 }
 
-
-//Average
-}
-
-let avrge = findAvg(8,20);
+let avrge = findAvg(8, 20);
 console.log(avrge);
-function findAvg( a, b,)
-{
-
-var avrge = (a + b) / 2;
-return avrge;
-
+function findAvg(a, b) {
+  var avrge = (a + b) / 2;
+  return avrge;
 }
 
 //Now that we have exercised hoisting, let's practice scoping.
 
-let fruits = ['pear', 'apple', 'grape'];
+let fruits = ["pear", "apple", "grape"];
 
+function printFruit() {
+  let favFruit = fruits[2];
 
+  console.log(fruits[0]);
+  printFavfruit();
 
-function printFruit()
-{
-console.log(fruits[0]);
-}
-function printFavFruit()
-{
+  function printFavfruit() {
     console.log(favFruit);
+    
+  }
+  console.log(leastFav);
+  
 }
+let leastFav = fruits[1];
+itslit();
+function itslit() {
+    console.log("Hello,Billy")
+}
+alertpopup();
+function alertpopup()
+{
+    alert("ITS LITT");
+}
+
 
 printFruit();
-
-
-
+printFavFruit();
